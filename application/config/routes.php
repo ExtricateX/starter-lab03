d74e358774c6d6e66fe56e5aad898043a82a0ce6
a00860087 <a00860087@SE12L0319-07.ad.bcit.ca>
2015-09-24 15:38:20 -0700

    Changed show/3 to a wildcard [show/(:num)] and rerouted to first/gimme/3. Also made a gimme method inside 'first' controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -37,6 +37,7 @@
 | in the URL cannot be matched to a valid route.
 |
 */
+$route['show/(:num)'] = "first/gimme/3";
 $route['lock/(:any)/(:any)'] = "welcome/shucks";
 $route['sleep'] = "first/zzz";
 $route['default_controller'] = "welcome";
